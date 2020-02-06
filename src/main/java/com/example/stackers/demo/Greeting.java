92aba18309ac05a74176a1ac9c71289c2ebd534a
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-06 18:23:26 -0500

    Building Greeting method

--- src/main/java/com/example/stackers/demo/Greeting.java
+++ src/main/java/com/example/stackers/demo/Greeting.java
@@ -4,7 +4,11 @@ public class Greeting {
 
     private final long id;
     private final String content;
-    
+
+    public Greeting(long id String content) {
+        this.id = id;
+        this.content = content;
+    }
 
 
 }
