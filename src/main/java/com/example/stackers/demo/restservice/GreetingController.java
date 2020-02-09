f75f69d01c1388f7d0669ca50724086be40de802
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-08 20:21:56 -0500

    Now imitating guide line for line, placement not 100 percent though in terms of packages

--- src/main/java/com/example/stackers/demo/restservice/GreetingController.java
+++ src/main/java/com/example/stackers/demo/restservice/GreetingController.java
@@ -1,5 +1,8 @@
 package com.example.stackers.demo.restservice;
 
+import com.example.stackers.demo.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -8,4 +11,9 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GreetingController {
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
