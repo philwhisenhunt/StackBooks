b767ca83c77d7206b0f3fb9253b9dd6d3374adaa
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-08 20:16:13 -0500

    rolling along now

--- src/main/java/com/example/stackers/demo/restservice/GreetingController.java
+++ src/main/java/com/example/stackers/demo/restservice/GreetingController.java
@@ -2,6 +2,10 @@ package com.example.stackers.demo.restservice;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class GreetingController {
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
 }
