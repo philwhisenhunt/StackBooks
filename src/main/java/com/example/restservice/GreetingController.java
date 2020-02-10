9997ed1da77af06f3a4577b63cef05c5422d3243
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-10 15:21:50 -0500

    Got it! Now it is displaying the counter as it changes

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "id", defaultValue = "0") Long id){
-            return new Greeting(counter.incrementAndGet(), String.format(template, name, id));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+            return new Greeting(counter.incrementAndGet(), String.format(template, name, counter));
     }
 }
